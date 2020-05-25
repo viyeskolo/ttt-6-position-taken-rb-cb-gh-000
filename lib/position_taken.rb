a889d314ad02ae7ac37f59e6ace31b9869705beb
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-25 19:59:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,15 @@
-# code your #position_taken? method here!
-ttt_board = ["",""," "," ","X"," "," "," ",""]
-
-def position_taken?(board, index)! (board[index] ==  " " || board[index] == "" || board[index] == nil)
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
 end
 
-puts position_taken?(ttt_board, 0)
-puts position_taken?(ttt_board, 1)
-puts position_taken?(ttt_board, 2)
-puts position_taken?(ttt_board, 3)
-puts position_taken?(ttt_board, 4)
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
+  end
+end
