b8fa5cfbcb94814f56e4aae89bcb60674040ecbd
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-25 19:57:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,26 +1,36 @@
-# code your #position_taken? method here!
-ttt_board = ["",""," "," ","X"," "," "," ",""]
-
-def position_taken?(board, index)! (board[index] ==  " " || board[index] == "" || board[index] == nil)
-end
-
-puts position_taken?(ttt_board, 0)
-puts position_taken?(ttt_board, 1)
-puts position_taken?(ttt_board, 2)
-puts position_taken?(ttt_board, 3)
-puts position_taken?(ttt_board, 4)
-ttt_board = ["","X"," "," "," "," "," "," "," "]
+# this "@" is called an "instance variable" and it can make your code work
+# instance variables can be modified
+@ttt_board = ["","X"," "," "," "," "," "," "," "]
 puts "Enter a position on the board 1-9:"
 user_input = gets.chomp
 number_entered = user_input.to_i - 1
 
-def valid_move?(number_entered, board)
-  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+def valid_move?(number_entered)
+  number_entered.between?(0, 8) && !(position_taken?(@ttt_board, number_entered))
 end
 
 def position_taken?(board, index)
  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
 
-valid_move?(number_entered, ttt_board)
+puts valid_move?(number_entered)
+
+puts #blank space
+puts #blank space
+
+# this capitol "Z" is called an "constant variable" and it can make your code work
+# constance variables can *not* be modified
+Zttt_board = ["","X"," "," "," "," "," "," "," "]
+puts "Enter a position on the board 1-9:"
+user_input = gets.chomp
+number_entered = user_input.to_i - 1
+
+def valid_move?(number_entered)
+  number_entered.between?(0, 8) && !(position_taken?(Zttt_board, number_entered))
 end
+
+def position_taken?(board, index)
+ !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+puts valid_move?(number_entered)
